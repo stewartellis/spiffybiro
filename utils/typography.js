ccebefe5a04bba85761bc2d8f38151baf853683a
Stewart Ellis <stewart@sharksoda.com>
2017-02-12 16:49:09 -0500

    initial typography

--- utils/typography.js
+++ utils/typography.js
@@ -1,7 +1,16 @@
 import Typography from 'typography'
-import Wordpress2016 from 'typography-theme-wordpress-2016'
+//import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-const typography = new Typography(Wordpress2016)
+//const typography = new Typography(Wordpress2016)
+
+const typography = new Typography({
+  baseFontSize: '20px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['droid serif', 'sans-serif'],
+  bodyFontFamily: ['droid sans', 'serif'],
+  // See below for the full list of options.
+})
+
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
